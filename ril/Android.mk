81e4ffe45cbdad5b1c579f254a670aa887209b7c
sparx <alliancesparx@gmail.com>
2017-10-12 22:11:44 +0100

    Remove rmt_storage_shim

--- ril/Android.mk
+++ ril/Android.mk
@@ -1,27 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
 LOCAL_SRC_FILES := ril_shim.c
-
 LOCAL_SHARED_LIBRARIES := liblog libcutils libbinder libutils
-
 LOCAL_MODULE := libshim_ril
-
 LOCAL_MODULE_TAGS := optional
-
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_SHARED_LIBRARIES := liblog libcutils libutils
-
-LOCAL_SRC_FILES := \
-    rmt_storage_shim.c
-
-LOCAL_MODULE := librmt_shim
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-
 include $(BUILD_SHARED_LIBRARY)
